fc715f60f38aa4e9a8328ec9f774eb337f67e5af
Pete Emerson <pete@appnexus.com>
2013-09-06 15:48:11 -0700

    Remove duplicate restart

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ import argparse
 parser = argparse.ArgumentParser(description='Copy files to the CoreOS VM')
 parser.add_argument('--debug', default=False, action='store_true')
 parser.add_argument('--backup', default=False, action='store_true')
+parser.add_argument('--commander', default=False, action='store_true')
 parser.add_argument('--reconfigure-etcd', default=False, action='store_true')
+parser.add_argument('--disable-reboot', default=False, action='store_true')
 args = parser.parse_args()
 
 try:
@@ -35,25 +37,34 @@ if not okay:
 	sys.exit(1)
 
 if args.backup:
-	ssh_command = ['scp', '-r', '-i', config['IdentityFile'], '-P', config['Port'], config['User'] + '@' + config['HostName'] + ':commander', '.']
-else:
-	ssh_command = ['scp', '-r', '-i', config['IdentityFile'], '-P', config['Port'], 'commander', config['User'] + '@' + config['HostName'] + ':']
-print ' '.join(ssh_command)
-if not args.debug:
-	result = subprocess.call(ssh_command)
+	ssh_command = ['scp', '-o', 'LogLevel=quiet', '-r', '-i', config['IdentityFile'], '-P', config['Port'], config['User'] + '@' + config['HostName'] + ':commander', '.']
+	print ' '.join(ssh_command)
+	if not args.debug:
+		result = subprocess.call(ssh_command)
+elif args.commander:
+	ssh_command = ['scp', '-o', 'LogLevel=quiet', '-r', '-i', config['IdentityFile'], '-P', config['Port'], 'commander', config['User'] + '@' + config['HostName'] + ':']
+	print ' '.join(ssh_command)
+	if not args.debug:
+		result = subprocess.call(ssh_command)
 
 if args.reconfigure_etcd:
-	ssh_command = ['scp', '-i', config['IdentityFile'], '-P', config['Port'], 'coreos/etcd.service', config['User'] + '@' + config['HostName'] + ':']
+	ssh_command = ['scp', '-o', 'LogLevel=quiet', '-i', config['IdentityFile'], '-P', config['Port'], 'coreos/etcd-local.service', config['User'] + '@' + config['HostName'] + ':']
 	print ' '.join(ssh_command)
 	if not args.debug:
 		result = subprocess.call(ssh_command)
 
-	ssh_command = ['ssh', '-i', config['IdentityFile'], '-p', config['Port'], config['User'] + '@' + config['HostName'], 'sudo', 'cp', './etcd-local.service', '/media/state/units']
+	ssh_command = ['ssh', '-o', 'LogLevel=quiet', '-i', config['IdentityFile'], '-p', config['Port'], config['User'] + '@' + config['HostName'], 'sudo', 'cp', './etcd-local.service', '/media/state/units']
 	print ' '.join(ssh_command)
 	if not args.debug:
 		result = subprocess.call(ssh_command)
 
-	ssh_command = ['ssh', '-i', config['IdentityFile'], '-p', config['Port'], config['User'] + '@' + config['HostName'], 'sudo', 'systemctl', 'restart', 'local-enable.service']
+	ssh_command = ['ssh', '-o', 'LogLevel=quiet', '-i', config['IdentityFile'], '-p', config['Port'], config['User'] + '@' + config['HostName'], 'sudo', 'systemctl', 'restart', 'local-enable.service']
+	print ' '.join(ssh_command)
+	if not args.debug:
+		result = subprocess.call(ssh_command)
+
+if args.disable_reboot:
+	ssh_command = ['ssh', '-o', 'LogLevel=quiet', '-i', config['IdentityFile'], '-p', config['Port'], config['User'] + '@' + config['HostName'], 'sudo', 'systemctl', 'stop', 'update-engine-reboot-manager.service']
 	print ' '.join(ssh_command)
 	if not args.debug:
 		result = subprocess.call(ssh_command)
